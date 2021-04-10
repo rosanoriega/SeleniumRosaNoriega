e3c737d2e4f065a5082793f010ebd574e7a861f2
rosam.noriega@gmail.com <rosam.noriega@gmail.com>
2021-04-10 11:03:37 -0300

    Subir Ejercicio 2

---
+++ Ejercicio2.java
@@ -0,0 +1,18 @@
+package Ejercicios_Clase_4;
+
+public class Ejercicio2 {
+
+    public static void main(String [] args){
+
+        String [] días = new String[7];
+
+        días [0] = "Domingo";
+        días [1] = "Lunes";
+        días [2] = "Martes";
+        días [3] = "Miércoles";
+        días [4] = "Jueves";
+        días [5] = "Viernes";
+        días [6] = "Sábado";
+
+    }
+}
